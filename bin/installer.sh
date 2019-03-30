2f21df53005a9b12a9883ace4f640d1dc0a4c906
Mishra <vismis@f45c89af2de3.ant.amazon.com>
2019-03-30 23:46:18 +0100

    Fix stupid mistake

--- bin/installer.sh
+++ bin/installer.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-sudo apt-get install libtiff5-dev libjpeg8-dev zlib1g-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
 pip install -r ../requirements.txt --no-index
--- bin/run_test.py
+++ bin/run_test.py
@@ -4,10 +4,14 @@ import os
 def run_all_test():
 
     current_directory = os.getcwd()
-    file_name = os.environ('result_file_name') or "result"
-    result_folder_name = os.environ("result_folder_name") or "../" + current_directory
+    file_name = os.environ.get('result_file_name') or "result"
+    result_folder_name = os.environ.get("result_folder_name") or "../" + current_directory
 
-    command = 'pytest' + "../tst" + ' --junit-xml=' + result_folder_name + \
+    run_command = 'pytest ' + "../tst" + ' --junit-xml=' + result_folder_name + \
               '/' + file_name + '.xml'
-    os.system(command=command)
+    os.system(run_command)
+
+
+if __name__ == '__main__' :
+    run_all_test()
 
